b523abe26576e5c79b3335845cfc64ba9104b55a
Sahilmahalle <sahilvmahalle@gmail.com>
2024-02-05 22:01:33 +0530

    pages added

--- valentine/src/App.jsx
+++ valentine/src/App.jsx
@@ -1,53 +1,18 @@
-import React, { useState } from "react";
-import "./App.css";
-
-const buttonTexts = [ "No",
-"Please yaar",
-"Think Again",
-"Are you sure ?",
-"I am a good boy",
-"I will be very angry",
-"I will be very very angry",
-"Ok fine i dont care",
-"I dont back off so easily ",]; // Add your array of texts
+import React from "react";
+import "./Home.css";
 
 function App() {
-  const [noButtonText, setNoButtonText] = useState(buttonTexts[0]);
-  const [yesClicked, setYesClicked] = useState(false);
-  const [noClicked, setNoClicked] = useState(false);
-
-  const handleNoButtonClick = () => {
-    const randomIndex = Math.floor(Math.random() * buttonTexts.length);
-    setNoButtonText(buttonTexts[randomIndex]);
-    setNoClicked(true);
-  };
-
-  const handleYesButtonClick = () => {
-    setYesClicked(true);
-  };
-
   return (
     <div className="container">
-      <div>
-        {/* Replace the src attribute with the URL of your image */}
-        <img src="./assets/img1.gif" alt="Valentine's Day" />
+      <h1>Happy Valentine Day</h1>
+      <div id="container-button">
+        <div>
+          <button>Check Your Love Score</button>
+        </div>
+        <div>
+          <button>Will You Be My Valentine</button>
+        </div>
       </div>
-      <h1>Will you be my valentine</h1>
-      {yesClicked ? (
-        <p>Thank you for your response!</p>
-      ) : (
-        <>
-          <button
-            className={`no-button ${noClicked ? "increase-size" : ""}`}
-            onClick={handleNoButtonClick}
-          >
-            {noButtonText}
-          </button>
-          <button className="yes-button" onClick={handleYesButtonClick}>
-            Yes
-          </button>
-        </>
-      )}
     </div>
   );
 }
---
+++ valentine/src/Ask.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import "./Ask.css";
+
+const buttonTexts = [
+  "No",
+  "Please yaar",
+  "Think Again",
+  "Are you sure ?",
+  "I am a good boy",
+  "I will be very angry",
+  "I will be very very angry",
+  "Ok fine I don't care",
+  "I don't back off so easily ",
+];
+
+const initialImage =
+  "https://media.tenor.com/NhawLnedTFEAAAAj/love-milk-and-mocha-bear.gif"; // Use your image URL
+
+function Ask() {
+  const [noButtonText, setNoButtonText] = useState(buttonTexts[0]);
+  const [yesClicked, setYesClicked] = useState(false);
+  const [noClicked, setNoClicked] = useState(false);
+  const [imageSrc, setImageSrc] = useState(initialImage);
+
+  const handleNoButtonClick = () => {
+    const randomIndex = Math.floor(Math.random() * buttonTexts.length);
+    setNoButtonText(buttonTexts[randomIndex]);
+    setNoClicked(true);
+  };
+
+  const handleYesButtonClick = () => {
+    setYesClicked(true);
+    setImageSrc("https://media.tenor.com/7vQ4wX6QVewAAAAj/kiss.gif"); // Use your image URL
+  };
+
+  return (
+    <div className="container">
+      <div>
+        {/* Replace the src attribute with the URL of your image */}
+        <img src={imageSrc} alt="Valentine's Day" />
+      </div>
+      <h1>Will you be my valentine</h1>
+      {yesClicked ? (
+        <p>Thank you for your response!</p>
+      ) : (
+        <>
+          <button
+            className={`no-button ${noClicked ? "increase-size" : ""}`}
+            onClick={handleNoButtonClick}
+          >
+            {noButtonText}
+          </button>
+          <button className="yes-button" onClick={handleYesButtonClick}>
+            Yes
+          </button>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Ask;
